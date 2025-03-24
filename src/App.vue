45d88e4d5573daf1eb01243934fb8d7886e50ea6
m.irfan <m.irfanwebdev@gmail.com>
2025-03-24 20:54:55 +0700

    add hero section

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,18 @@
 // import TheWelcome from './components/TheWelcome.vue'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import SectionHero from '@/components/SectionHero.vue';
 </script>
 
 <template>
   <header>
     <Header />
   </header>
-  
+
+  <section class="bg-white/25">
+    <SectionHero />
+  </section>
+
   <footer class="bg-secondary-blue text-white">
     <Footer />
   </footer>
---
+++ src/components/SectionHero.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Button from './Button.vue';
+</script>
+
+<template>
+    <div class="flex flex-wrap justify-between items-center py-4 mx-4 lg:py-[92px] lg:ms-[150px] lg:me-[60px]">
+        <div class="flex flex-col gap-4 lg:w-[450px] relative">
+            <div>
+                <h3 class="text-primary-red font-semibold text-2xl mb-3">We Are Here</h3>
+                <h1 class="text-5xl">IT Consultant & Outsourcing</h1>
+            </div>
+            <p>
+                We believe that technology is for every business.
+                From roadside stall business, to publicy listed business.
+                Elevate your business with technology.
+            </p>
+            <Button>Contact Us</Button>
+
+            <!-- circle -->
+            <div
+                class="absolute top-0 left-0 transform -translate-1/2 w-[200px] h-[200px] bg-primary-red/10 rounded-full">
+            </div>
+        </div>
+        <div class="lg:w-[700px]">
+            <img class="w-full" src="@/assets/hero.png" alt="hero">
+        </div>
+    </div>
+</template>
