9401f5a0278a247127a44c3bc2878036a0e14111
m.irfan <m.irfanwebdev@gmail.com>
2025-03-24 19:27:05 +0700

    add button and menu component

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    variant: {
+        type: String,
+        default: 'primary'
+    },
+    href: {
+        type: String
+    }
+});
+</script>
+
+<template>
+    <template v-if="variant === 'white'">
+        <a href="{{ href }}" class="rounded-md bg-white hover:bg-primary-red text-primary-red hover:text-white  py-2 px-4">
+            <slot></slot>
+        </a>
+    </template>
+    <template v-else>
+        <a href="{{ href }}" class="rounded-md bg-primary-red hover:bg-white text-white hover:text-primary-red py-2 px-4">
+            <slot></slot>
+        </a>
+    </template>
+</template>
---
+++ src/components/MenuServices.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    detail: {
+        type: String,
+        required: true
+    },
+});
+</script>
+
+<template>
+    <div class="flex flex-col gap-5 p-[50px] bg-white hover:bg-primary-red hover:opacity-50 relative">
+        <slot name="icon"></slot>
+        <h3 class="font-semibold text-xl">{{ title }}</h3>
+        <p>{{ detail }}</p>
+    </div>
+</template>
