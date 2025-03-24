e977ab642af49ade6a1d19e3c76a7896d6d9e4a4
m.irfan <m.irfanwebdev@gmail.com>
2025-03-24 22:01:38 +0700

    add about and tech section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import SectionHero from '@/components/SectionHero.vue';
+import SectionAbout from '@/components/SectionAbout.vue';
 </script>
 
 <template>
@@ -13,6 +12,21 @@ import SectionHero from '@/components/SectionHero.vue';
 
   <section class="bg-white/25">
     <SectionHero />
+  </section>
+
+  <section>
+    <SectionAbout />
+  </section>
+
+  <section class="w-full lg:h-100 bg-primary-red text-white relative">
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+      <h3 class="text-2xl">Our Purpose</h3>
+      <h1 class="text-5xl">Technology Inclusivity</h1>
+    </div>
+
+    <div class="absolute top-0 right-0 h-full">
+      <img class="h-full" src="@/assets/tech.svg" alt="tech">
+    </div>
   </section>
 
   <footer class="bg-secondary-blue text-white">
---
+++ src/components/SectionAbout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Button from './Button.vue';
+</script>
+<template>
+    <div class="mx-auto lg:ms-[150px] py-6 lg:pt-[162px] lg:pb-[102px]">
+        <div class="w-[300px] lg:w-[856px] relative">
+            <img class="w-full" src="@/assets/office.png" alt="about-illustration">
+
+            <div
+                class="absolute max-w-100 top-0 right-0 transform translate-x-3/4 translate-y-1/4 p-12 bg-primary-red/90 text-white">
+                <h1 class="text-5xl font-semibold mb-5">About Us</h1>
+                <p class="mb-8 lg:text-lg">
+                    As an Information Information Technology Outsourcing or (IT Outsourcing) company, we help your
+                    business to grow through qualified human resources.
+                </p>
+                <Button variant="white">About Our Company</Button>
+            </div>
+        </div>
+    </div>
+</template>
